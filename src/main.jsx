887cf3e4cc291a6a49f9167880e9257e31f85809
Claudio Piazzai <claudio@Claudios-MacBook-Pro.local>
2025-08-21 15:53:56 +0200

    Use HashRouter for GitHub Pages + rebuild

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // sicuro su GitHub Pages
+import App from "./App.jsx";
+import "./index.css"; // assicurati di importare i CSS globali
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
-)
+);
